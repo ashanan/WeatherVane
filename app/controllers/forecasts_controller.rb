bee8e9a76027731981433c07aa034a839c15db0b
Avner Shanan <avner@hey.com>
2024-04-25 15:03:00 -0500

    Stub out form response with view

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -3,6 +3,10 @@ class ForecastsController < ApplicationController
   end
 
   def create
+    redirect_to forecasts_path
+  end
+
+  def show
     @response = Forecast.new(temperature: 75)
   end
 end
