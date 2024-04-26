fc032806e1cf66dcacf0c3a5a9124b6fdd19b023
Avner Shanan <avner@hey.com>
2024-04-26 14:20:35 -0500

    Resolve linter issues

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -4,17 +4,17 @@ require 'rails_helper'
 
 RSpec.describe Forecast, type: :model do
   it 'initializes when no parameters are passed in' do
-    expect { Forecast.new }.not_to raise_error(NoMethodError)
+    expect { described_class.new }.not_to raise_error(NoMethodError)
   end
 
   describe '#from_cache' do
     it 'returns true when initialized as coming from the cache' do
-      forecast = Forecast.create(zip: 60_613, temperature: 75, from_cache: true)
+      forecast = described_class.create(zip: 60_613, temperature: 75, from_cache: true)
       expect(forecast.from_cache).to be_truthy
     end
 
     it 'returns false by default' do |_variable|
-      forecast = Forecast.create(zip: 60_613, temperature: 75)
+      forecast = described_class.create(zip: 60_613, temperature: 75)
       expect(forecast.from_cache).to be_falsy
     end
   end
