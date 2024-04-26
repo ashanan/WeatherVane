afbbd03f86912bba7d85845e68bd49f634bbe453
Avner Shanan <avner@hey.com>
2024-04-26 14:34:36 -0500

    Disable cop in temp code

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -4,7 +4,7 @@
 # For additional details, see:
 #   [ADR 3](../architecture/adr/0003-encapsulate-cache-and-external-api-inside-a-service-class.md)
 class WeatherService
-  @@data = {}
+  @@data = {} # rubocop:disable Style/ClassVars (we can do this here because this is temporary code)
 
   def self.forecast(zip)
     @@data[zip] = Forecast.new(temperature: rand(85), zip:) if @@data[zip].nil?
